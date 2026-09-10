fc88e05895df816e803fa98754bb126af5264c87
MATRIX-IA-BETA <fabiocsgbrasil@gmail.com>
2026-09-10 20:38:40 -0300

    Monta status do relatório financeiro Mercado Pago

--- src/routes/finance-ml-funds.js
+++ src/routes/finance-ml-funds.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
 router.use(require("./finance-ml-sync-v3"));
 router.use(require("./finance-liabilities"));
+router.use(require("./finance-mp-release-report").router);
 module.exports = router;
