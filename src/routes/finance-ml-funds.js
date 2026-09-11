dc3071dde9c94ac9157ba0488580ade8470964a8
MATRIX-IA-BETA <fabiocsgbrasil@gmail.com>
2026-09-11 08:32:18 -0300

    Ativa desvinculação do MP da Pluggy

--- src/routes/finance-ml-funds.js
+++ src/routes/finance-ml-funds.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+router.use(require("./finance-mp-detach-pluggy"));
 router.use(require("./finance-mp-balance-direct"));
 router.use(require("./finance-open-finance-mp-filter"));
 router.use(require("./finance-mp-authoritative-sync"));
