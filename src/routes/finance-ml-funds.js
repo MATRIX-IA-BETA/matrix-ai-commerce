f86ecec6fc04fe493cf725f0ef75b8a30f37c025
MATRIX-IA-BETA <fabiocsgbrasil@gmail.com>
2026-09-10 21:15:33 -0300

    Usa atualização ao vivo Pluggy no botão Atualizar

--- src/routes/finance-ml-funds.js
+++ src/routes/finance-ml-funds.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
 router.use(require("./finance-mp-authoritative-sync"));
+router.use(require("./finance-open-finance-live-refresh"));
 router.use(require("./finance-liabilities"));
 module.exports = router;
