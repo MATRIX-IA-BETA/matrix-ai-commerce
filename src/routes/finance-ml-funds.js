c13e7511efe31651d278516c305139bea368f3b0
MATRIX-IA-BETA <fabiocsgbrasil@gmail.com>
2026-09-13 20:27:04 -0300

    Ativa reconciliação V4 do Mercado Pago

--- src/routes/finance-ml-funds.js
+++ src/routes/finance-ml-funds.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 router.use(require("./finance-mp-detach-pluggy"));
 router.use(require("./finance-open-finance-mp-filter"));
-router.use(require("./finance-mp-reconciliation-v3"));
+router.use(require("./finance-mp-reconciliation-v4"));
 router.use(require("./finance-open-finance-live-refresh"));
 router.use(require("./finance-mp-mlmatch-diagnostic"));
 router.use(require("./finance-liabilities"));
